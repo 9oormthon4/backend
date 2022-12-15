7dad478a3001f135add40b9b68f4e9876d6a579a
iamnardis <jukanvia1@gmail.com>
2022-12-15 21:02:10 +0900

    fix:cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from './error/filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
 
   app.useGlobalFilters(
     HttpExceptionFilter(false)
