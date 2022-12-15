33aadd0038dd1ae499480b54eaaca0e83e2900a8
iamnardis <jukanvia1@gmail.com>
2022-12-15 15:25:20 +0900

    fix: dto

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './error/filter'
+import { ValidationPipe } from '@nestjs/common' 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,10 @@ async function bootstrap() {
   app.enableCors({
     origin: true
   });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
 
   const config = new DocumentBuilder()
     .setTitle('Adregamdy example')
